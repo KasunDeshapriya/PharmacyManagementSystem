863b668466b8a1c8b20a925f72eb9be551d22dfc
krdeshapriya <krdeshapriya@gmail.com>
2018-07-24 16:26:48 +0530

    Configuring Project Stag 2

---
+++ PharmacyManagementBackend/src/main/java/net/kr/PharmacyManagementBackend/App.java
@@ -0,0 +1,13 @@
+package net.kr.PharmacyManagementBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
